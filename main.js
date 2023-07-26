248567af6eb4d56d1242769115beebf803a72ed9
PALAKURI KUMARA SWAMY <19r21a0498@mlrinstitutions.ac.in>
2023-07-26 17:56:43 +0530

    total update

---
+++ main.js
@@ -0,0 +1,15 @@
+let c=document.getElementById("p_c")
+let s=document.getElementById("save")
+let count=0;
+let st="N0";
+function myfunc(){
+  count=count+1;
+  c.innerText=count;
+  console.log(c);
+}
+function mysav(){
+  st=st+" :"+count;
+  s.innerText=st;
+  count=0;
+  console.log(c);
+}
